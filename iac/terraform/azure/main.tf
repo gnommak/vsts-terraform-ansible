3c74157eba719dc7dee4443df6dd5bab49dcee3b
Oleh <oleh@Olehs-MBP.local>
2019-06-16 23:35:22 +0300

    Just Do IT 4

--- iac/terraform/azure/main.tf
+++ iac/terraform/azure/main.tf
@@ -1,5 +1,15 @@
 terraform {
   required_version = ">= 0.12"
+
+  backend "azurerm" {
+    storage_account_name = "${var.s_a_name}"
+    container_name       = "${var.c_name}"
+    key                  = "dev.terraform.tera"
+
+    # rather than defining this inline, the Access Key can also be sourced
+    # from an Environment Variable - more information is available below.
+    access_key = "${var.a_key}"
+  }
 }
 
 # Configure the Microsoft Azure Provider
