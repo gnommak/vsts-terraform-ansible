cce30078cc82fdf61f429494cc91c5bdf5c5fa0d
Oleh <oleh@Olehs-MBP.local>
2019-06-17 00:13:44 +0300

    dgdh

--- iac/terraform/init.sh
+++ iac/terraform/init.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 echo "************* execute terraform init"
 ## execute terrafotm build and sendout to packer-build-output
-## export ARM_CLIENT_ID=$1
-## export ARM_CLIENT_SECRET=$2
-## export ARM_SUBSCRIPTION_ID=$3
-## export ARM_TENANT_ID=$4
-## export ARM_ACCESS_KEY=$5
+export TF_WS=/home/vsts/work/r1/a/_Diploma_gnommak-CI/drop/iac/terraform/azure/workspaces.tf
 
-touch /home/vsts/work/r1/a/_Diploma_gnommak-CI/drop/iac/terraform/azure/backend.tfvars
-echo arm_client_id = \"$ARM_CLIENT_ID\" >> /home/vsts/work/r1/a/_Diploma_gnommak-CI/drop/iac/terraform/azure/backend.tfvars
-echo arm_client_secret = \"$ARM_CLIENT_SECRET\" >> /home/vsts/work/r1/a/_Diploma_gnommak-CI/drop/iac/terraform/azure/backend.tfvars
-echo arm_subscription_id = \"$ARM_SUBSCRIPTION_ID\" >> /home/vsts/work/r1/a/_Diploma_gnommak-CI/drop/iac/terraform/azure/backend.tfvars
-echo arm_tenant_id = \"$ARM_TENANT_ID\" >> /home/vsts/work/r1/a/_Diploma_gnommak-CI/drop/iac/terraform/azure/backend.tfvars
-echo a_key = \"$ARM_ACCESS_KEY\" >> /home/vsts/work/r1/a/_Diploma_gnommak-CI/drop/iac/terraform/azure/backend.tfvars
-echo ssh_key = \"$SSH_PUB_KEY\" >> /home/vsts/work/r1/a/_Diploma_gnommak-CI/drop/iac/terraform/azure/backend.tfvars
-echo s_a_name = \"$STORAGE_ACCOUNT_NAME\" >> /home/vsts/work/r1/a/_Diploma_gnommak-CI/drop/iac/terraform/azure/backend.tfvars
-echo c_name = \"$CONTAINER_NAME\" >> /home/vsts/work/r1/a/_Diploma_gnommak-CI/drop/iac/terraform/azure/backend.tfvars
+touch $TF_WS
+echo "terraform {" >> $TF_WS
+echo "  backend \"azurerm\" {" >> $TF_WS
+echo arm_client_id = \"$ARM_CLIENT_ID\" >> $TF_WS
+echo arm_client_secret = \"$ARM_CLIENT_SECRET\" >> $TF_WS
+echo arm_subscription_id = \"$ARM_SUBSCRIPTION_ID\" >> $TF_WS
+echo arm_tenant_id = \"$ARM_TENANT_ID\" >> $TF_WS
+echo a_key = \"$ARM_ACCESS_KEY\" >> $TF_WS
+echo ssh_key = \"$SSH_PUB_KEY\" >> $TF_WS
+echo s_a_name = \"$STORAGE_ACCOUNT_NAME\" >> $TF_WS
+echo c_name = \"$CONTAINER_NAME\" >> $TF_WS
 
-cut /home/vsts/work/r1/a/_Diploma_gnommak-CI/drop/iac/terraform/azure/backend.tfvars
+ls /home/vsts/work/r1/a/_Diploma_gnommak-CI/drop/iac/terraform/azure/
+cat $TF_WS
 
 echo "Run"
-## terraform init  -backend-config=backend.tfvars
