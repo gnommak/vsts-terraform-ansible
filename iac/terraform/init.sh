315a2f1966940645a52e440697f4508f329da3cf
Oleh <oleh@Olehs-MBP.local>
2019-06-17 00:23:03 +0300

    asdfcie

--- iac/terraform/init.sh
+++ iac/terraform/init.sh
@@ -6,14 +6,12 @@ export TF_WS=/home/vsts/work/r1/a/_Diploma_gnommak-CI/drop/iac/terraform/azure/w
 touch $TF_WS
 echo "terraform {" >> $TF_WS
 echo "  backend \"azurerm\" {" >> $TF_WS
-echo arm_client_id = \"$ARM_CLIENT_ID\" >> $TF_WS
-echo arm_client_secret = \"$ARM_CLIENT_SECRET\" >> $TF_WS
-echo arm_subscription_id = \"$ARM_SUBSCRIPTION_ID\" >> $TF_WS
-echo arm_tenant_id = \"$ARM_TENANT_ID\" >> $TF_WS
-echo a_key = \"$ARM_ACCESS_KEY\" >> $TF_WS
-echo ssh_key = \"$SSH_PUB_KEY\" >> $TF_WS
-echo s_a_name = \"$STORAGE_ACCOUNT_NAME\" >> $TF_WS
-echo c_name = \"$CONTAINER_NAME\" >> $TF_WS
+echo "    storage_account_name = \"$STORAGE_ACCOUNT_NAME\"" >> $TF_WS
+echo "    container_name       = \"$CONTAINER_NAME\""
+echo "    key                  = \"dev.terraform.tera\""
+echo "    access_key = \"$ARM_ACCESS_KEY\""
+echo "  }"
+echo "}"
 
 ls /home/vsts/work/r1/a/_Diploma_gnommak-CI/drop/iac/terraform/azure/
 cat $TF_WS
