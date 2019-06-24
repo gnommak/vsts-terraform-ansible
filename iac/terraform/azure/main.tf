f37bea147133115668c0f765843361a57c113447
Oleh <oleh@Olehs-MBP.local>
2019-06-24 21:40:37 +0300

    NEW?

--- iac/terraform/azure/main.tf
+++ iac/terraform/azure/main.tf
@@ -124,7 +124,7 @@ resource "azurerm_network_interface" "nic" {
 
 resource "azurerm_network_interface_backend_address_pool_association" "lbs" {
   network_interface_id    = "${azurerm_network_interface.nic[count.index].id}"
-  ip_configuration_name   = "ipconfigurationbackend"
+  ip_configuration_name   = "ipconfig${count.index}"
   backend_address_pool_id = "${azurerm_lb_backend_address_pool.backend_pool.id}"
   count                   = 2
 
