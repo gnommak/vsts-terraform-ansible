bb25785a600b6fa64ac5505dcf93ae0248ecc66e
Oleh <oleh@Olehs-MBP.local>
2019-06-24 21:33:14 +0300

    NEW

--- iac/terraform/azure/main.tf
+++ iac/terraform/azure/main.tf
@@ -122,10 +122,12 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_network_interface_backend_address_pool_association" "lb" {
+resource "azurerm_network_interface_backend_address_pool_association" "lbs" {
   network_interface_id    = "${azurerm_network_interface.nic[count.index].id}"
   ip_configuration_name   = "ipconfigurationbackend"
   backend_address_pool_id = "${azurerm_lb_backend_address_pool.backend_pool.id}"
+  count                   = 2
+
 }
 
 
